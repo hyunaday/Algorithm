ee3212695b0d7b7c9ac5397f806c901b1479cbae
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2024-07-30 17:48:07 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 112 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,27 +1,19 @@
-import java.util.Arrays;
-import java.util.Scanner;
- 
-public class Main {
-    public static void main(String args[]) {
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
         
-        Scanner sc = new Scanner(System.in);
-        
-        char[] text = sc.next().toCharArray();
-        char[] newText = new char[text.length];
-        // System.out.println(Arrays.toString(text));
-        
-        for (int i = 0; i < text.length; i++) {
-            if (text[i] <= 90) {                                // 대문자 Z 의 ASCII 코드인 90 을 기준으로 이보다 작은수는 대문자.
-                newText[i] = (char)(text[i] + 32);              // ASCII 코드에서 대문자와 소문자는 32 간격 차이를 갖음.
-            }
-            else if (text[i] >= 97) {                           // 소문자 a 의 ASCII 코드인 97 을 기준으로 이보다 큰수는 소문자.
-                newText[i] = (char)(text[i] - 32);              // ASCII 코드에서 대문자와 소문자는 32 간격 차이를 갖음.
-            }
+        char[] charArray = input.toCharArray();
+        for(int i=0; i<input.length(); i++){
+            char ch = input.charAt(i);
+            if('a' <= ch && ch <= 'z') charArray[i] = Character.toUpperCase(ch);
+            else charArray[i] =Character.toLowerCase(ch);            
         }
-        // System.out.println(Arrays.toString(newText));           // TEST PRINT
         
-        for (int i = 0; i < newText.length; i++) {
-            System.out.print(newText[i]);
-        }
+        System.out.println(charArray);
     }
 }

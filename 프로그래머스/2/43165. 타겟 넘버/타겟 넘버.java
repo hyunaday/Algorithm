b97883b9090824956451de8a1e524c3073dbb81c
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2024-08-02 17:54:59 +0900

    [level 2] Title: 타겟 넘버, Time: 11.88 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,33 +1,17 @@
+import java.util.*;
 class Solution {
-    int[] numbers; // 사용할 수 있는 숫자가 담긴 배열 선언
-    int target;
-    static int answer;
-    
-    
-    void dfs(int idx, int sum){
-        // idx = dfs call에서 연산할 숫자가 위치한 인덱스
-        // sum = 누적합
- 
-        
-        // 1. 탈출 조건
-        if (idx == numbers.length){
-            if(sum==target) answer++;
-            return;
+    public int solution(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int num : numbers) {
+            Map<Integer, Integer> temp = new HashMap<>();
+            for (int key : map.keySet()) {
+                temp.put(key+num, map.get(key) + temp.getOrDefault(key+num, 0));
+                temp.put(key-num, map.get(key) + temp.getOrDefault(key-num, 0));
+            }
+            map = temp;
         }
-        
-        // 2. 수행동작
-        dfs(idx+1, sum + numbers[idx]);
-        dfs(idx+1, sum - numbers[idx]);
-        
-    }
-    
-    public int solution(int[] numbers, int target) { 
-        answer = 0;
-        this.numbers = numbers; // 멤버변수 Numbers에 전달 받은 numbers 담음
-        this.target = target; // 멤버 변수 target에 전달 받은 target 담음
- 
-        dfs(0,0);
-        
-        return answer;
+        return map.getOrDefault(target, 0);
     }
 }

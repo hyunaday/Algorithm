1e148357d29518af2db7acd3869763a5a39d717e
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2024-08-05 17:45:17 +0900

    [Bronze V] Title: 합, Time: 168 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,3 +1,6 @@
+//n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+//1부터 n까지 합을 출력한다.
+
 import java.util.Scanner;
  
 public class Main {
@@ -9,9 +12,9 @@ public class Main {
 		in.close();
 		int sum = 0;
         
-		for (int i = 1; i <= a; i++) {
-			sum += i;
-		}
+		for (int i = 1; i<=a; i++ ) {
+            sum +=i;
+        }
 		System.out.println(sum);
 	}
 }

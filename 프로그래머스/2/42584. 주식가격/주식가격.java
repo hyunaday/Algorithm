9fcdbd0e32afd8f985ab2eaebe796a7a2e1c3f2d
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2024-08-09 13:48:34 +0900

    [level 2] Title: 주식가격, Time: 18.26 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,10 +2,11 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        for (int i = 0; i<prices.length; i++) {
-            for (int j = i+1; j<prices.length; j++) {
+        for(int i = 0; i<prices.length; i++) {
+            for(int j = i+1; j<prices.length; j++) {
                 answer[i]++;
-                if(prices[i]>prices[j]) {
+                
+                if(prices[i]> prices[j]) {
                     break;
                 }
             }

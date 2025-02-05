e8ef792ab8aefef7e2a93d2b5d9bba99ca5075ae
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2025-02-05 11:32:32 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 660 ms, Memory: 93920 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,30 +1,30 @@
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
- 
+import java.io.InputStreamReader;
  
 public class Main {
 	public static void main(String[] args) throws IOException {
     
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
+        
+		/*
+		  -1000000 ~ 1000000
+		  기준점 0 = index 100000 으로 생각
+		*/
+		boolean[] arr = new boolean[2000001];	
+        
 		int N = Integer.parseInt(br.readLine());
-		
-		// list 계열 중 하나를 쓰면 된다.
-		ArrayList<Integer> list = new ArrayList<>();
-		
+        
 		for(int i = 0; i < N; i++) {
-			list.add(Integer.parseInt(br.readLine()));
+			arr[Integer.parseInt(br.readLine()) + 1000000] = true;
 		}
-		
-		Collections.sort(list);
-		
-		for(int value : list) {
-			sb.append(value).append('\n');
+ 
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i]) {
+				sb.append((i - 1000000)).append('\n');
+			}
 		}
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 }
